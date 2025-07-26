aaf64b0b571b0ea88f3ce3c213a1a42a7a32afc0
Darshan Raja <152625985+Darshan-raja@users.noreply.github.com>
2025-07-27 01:18:09 +0530

    Update ap.js
    USD price updated

--- Live price project/ap.js
+++ Live price project/ap.js
@@ -4,13 +4,14 @@ var eth = document.getElementById("ethereum");
 var doge = document.getElementById("dogecoin");
 function convert() {
     var usdAmount = parseFloat(document.getElementById('usd').value);
-    var result = usdAmount * 74.50; // Assuming 1 USD = 74.50 INR (You can change this value as per current exchange rate)
+    var result = usdAmount * 74.50; // Assuming 1 USD = 86.46  INR (You can change this value as per current exchange rate)
     document.getElementById('result').innerText = `${usdAmount} USD = ${result.toFixed(2)} INR`;
 }
 
 var liveprice = {
     "async": true,
     "scroosDomain": true,
+    //API url
     "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Clitecoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
 
     "method": "GET",
